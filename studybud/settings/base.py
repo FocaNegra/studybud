ab6a2adea6c6a13ff7a617fbcbe7ad32ac3f11cb
FocaNegra <victor.rn96@gmail.com>
2022-06-02 12:23:36 +0200

    solved secretkey issue

--- studybud/settings/base.py
+++ studybud/settings/base.py
@@ -12,20 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ax&p%k515c1ql8))0mg*q46-1-2(mo_j3++7r(ux7j$98uxqkb'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
